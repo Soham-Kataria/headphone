52655eb6e7078d0112226e9c25055c7f2bd1723b
soham-kataria-lw <sohamkataria.lw@gmail.com>
2026-05-06 15:21:18 +0530

    project configuration and base setup

---
+++ src/lib/gsap.ts
@@ -0,0 +1,6 @@
+// lib/gsap.ts
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { useGSAP } from "@gsap/react"
+gsap.registerPlugin(ScrollTrigger, useGSAP)
+export { gsap, ScrollTrigger, useGSAP }
